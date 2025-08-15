b1348d776099a192b3d285a178189418d9a5ed43
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2025-08-14 21:03:11 -0500

    Changes Files - Main

--- src/lib/SitesGallery.svelte
+++ src/lib/SitesGallery.svelte
@@ -2,29 +2,29 @@
   let activeIndex = 0;
   const panels = [
     {
-      title: "Explore The World",
+      title: "Beautiful Landscape",
       image:
-        "https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images.alphacoders.com/124/thumbbig-1249674.webp",
     },
     {
-      title: "Wild Forest",
+      title: "Serene Lake",
       image:
-        "https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images2.alphacoders.com/118/thumbbig-1185562.webp",
     },
     {
-      title: "Sunny Beach",
+      title: "Mountain View",
       image:
-        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80",
+        "https://images3.alphacoders.com/118/thumbbig-1185563.webp",
     },
     {
-      title: "City on Winter",
+      title: "Sunset Beach",
       image:
-        "https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
+        "https://images2.alphacoders.com/133/thumbbig-1330681.webp",
     },
     {
-      title: "Mountains - Clouds",
+      title: "City Skyline",
       image:
-        "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images4.alphacoders.com/754/thumbbig-754567.webp",
     },
   ];
 
@@ -65,5 +65,14 @@
     margin: 10px;
     display: block;
     /* ... otros estilos existentes ... */
+  }
+  h3 {
+    margin: 0.5rem;
+    padding: 0.5rem;
+    background-color: rgba(55, 77, 124, 0.651);
+    border-radius: 0.5rem;
+  }
+  .title {
+    background-color: rgba(14, 40, 59, 0.418);
   }
 </style>
